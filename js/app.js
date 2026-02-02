202db1008b05cce6646815fb0a03c4f6c85a8896
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2026-02-02 09:11:39 -0400

    Update API_URL to point to the correct endpoint

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbwNEXOb0NjgZeork1u_hmWK7gZsyAYtRb1iPbn-YW6HZaaJh_SbjHUMwfTkf_QIeH5rxw/exec";
 
 // CONFIGURAÇÃO DOS CAMPOS
 const CAMPOS = {
